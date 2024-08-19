68e7f937d773ba9967c748c164718eba36468c14
FyX0X <martinergo06@gmail.com>
2024-08-19 02:23:10 +0200

    added new car physics

---
+++ car_class.py
@@ -0,0 +1,149 @@
+import pygame
+import numpy as np
+import math
+
+# New python file, for new car physics
+
+
+class Car:
+
+    def __init__(self, pos, img):
+        self.pos = pos
+        self.vel = np.zeros(2)
+        self.acceleration = np.zeros(2)
+        self.speed = 0
+        self.direction_vector = np.array([1, 0])
+        self.rotation = 0
+
+        self.reverse = False
+
+        self.engine_max_force = 200  # change value later
+        self.drag_coefficient = 0.005
+        self.friction_coefficient = self.drag_coefficient * 30
+        self.engine_braking = 0.5
+        self.mass = 0.5
+
+        self.img = img
+        self.img_rect = None
+        self.rotated_img = None
+
+        self.throttle = 0       # accelerating from -1 to 1
+        self.steering = 0       # turning from -1 to 1
+
+        self.delta_time = 0
+
+    def draw(self, win):
+        # Rotate car
+        self.rotated_img = pygame.transform.rotate(self.img, self.rotation)
+        self.img_rect = self.rotated_img.get_rect(center=self.pos)          # set coordinate origin in center
+
+        win.blit(self.rotated_img, self.img_rect)     # draw car
+
+    def move(self, throttle, steering, delta_time, tick):
+        self.throttle = throttle
+        self.steering = steering
+
+        self.delta_time = delta_time
+
+        # calculate acceleration
+        if self.speed != 0:
+            if not self.reverse:
+                if throttle > 0:
+                    self.accelerating()
+                elif throttle <= 0:
+                    self.braking()
+            else:
+                if throttle < 0:
+                    self.accelerating()
+                elif throttle >= 0:
+                    self.braking()
+        else:
+            if throttle > 0:
+                self.accelerating()
+            elif throttle < 0:
+                self.reverse = True
+                self.accelerating()
+
+
+        self.update()
+        print(f"{tick}: vel= {self.vel}, reverse= {self.reverse}")
+
+        """
+        # calculate velocity
+        self.update_vel()
+        # calculate position
+        self.update_pos()
+        """
+
+    def update(self):
+        self.direction_vector = self.get_direction_vector()
+        self.speed = self.get_speed()
+        self.rotation = self.get_rotation()
+
+    def get_direction_vector(self, velocity=None):
+        if velocity is None:
+            velocity = self.vel
+        speed = self.get_speed(velocity)
+        if speed != 0:
+            return velocity / speed
+        else:
+            return self.direction_vector
+
+    def get_rotation(self):
+        return math.degrees(math.atan2(self.direction_vector[0], -self.direction_vector[1])) - 90
+
+    def get_speed(self, velocity=None):
+        if velocity is None:
+            velocity = self.vel
+        return math.sqrt(velocity[0] ** 2 + velocity[1] ** 2)  # scalar speed
+
+    def accelerating(self):
+        traction_force = self.engine_max_force * self.throttle * self.direction_vector
+        friction_force = -self.friction_coefficient * self.vel
+        drag_force = -self.drag_coefficient * self.vel * self.speed
+
+        net_force = traction_force + friction_force + drag_force
+        self.acceleration = net_force / self.mass
+
+        # update vel
+        self.vel += self.acceleration * self.delta_time
+
+        # update pos
+        self.pos += self.vel * self.delta_time
+
+        # self.update_vel(acceleration)
+
+    def braking(self):
+        braking_force = self.engine_max_force * self.throttle * self.direction_vector   # throttle < 0: opposes motion
+        friction_force = -self.friction_coefficient * self.vel
+        drag_force = -self.drag_coefficient * self.vel * self.speed
+        engine_braking_force = - self.engine_braking * self.direction_vector
+
+        net_force = braking_force + friction_force + drag_force + engine_braking_force
+        self.acceleration = net_force / self.mass
+
+        # check if going to flip direction and updates vel
+        new_vel = self.vel + self.acceleration * self.delta_time
+        new_direction = self.get_direction_vector(new_vel)
+        if (new_direction == -1 * self.direction_vector).all():     # direction has been reversed
+            self.stop()
+        else:                                               # direction has not been reversed
+            self.vel = new_vel
+
+        # update pos
+        self.pos += self.vel * self.delta_time
+
+        # return acceleration
+
+    def update_vel(self):
+
+        self.vel += self.acceleration * self.delta_time
+
+    def update_pos(self):
+        self.pos += self.vel * self.delta_time
+
+    def stop(self):
+        self.vel = np.zeros(2)              # set velocity to 0
+        if self.reverse:                    # if reversed:
+            self.direction_vector *= 1      # -> flips direction vector
+        self.reverse = False                # set reverse to false
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import time
+#import time
 import pygame
 import os
 import numpy as np
 import math
 import keyboard
+import car_class
 
 pygame.init()
 
@@ -14,53 +15,145 @@ RACE_TRACK_IMG = pygame.image.load(os.path.join("imgs", "race_track.png"))
 CAR_IMG = pygame.transform.scale_by(pygame.image.load(os.path.join("imgs", "red_car.png")), 0.8)
 
 
-class Car:
+class Old_Car:
     IMG = CAR_IMG
 
     def __init__(self, pos):
         self.pos = np.array([float(pos[0]), float(pos[1])])  # 2d vector
         self.vel = np.zeros(2)  # 2d vector
         self.speed = 0  # scalar speed
-        self.acceleration = np.zeros(2)  # 2d vector
+        #self.acceleration = np.zeros(2)  # 2d vector
         self.direction_vector = np.array([1, 0])
         self.rotation = 180  # scalar angle
+        self.reverse = False
+        self.stopped = True
 
         self.engine_max_force = 200  # change value later
         self.drag_coef = 0.005
         self.friction_coef = self.drag_coef * 30
+        self.engin_braking = 0.5
         #self.friction_coef = 0.3  # change value later    friction is approx 30x drag
         self.mass = 0.5
 
         self.img_rect = None
         self.rotated_img = None
 
-    def move(self, gas: float = 0, turn: float = 0, delta_time: float = 1 / FPS, time = 0):
-        # calculate speed
-        self.speed = math.sqrt(self.vel[0] ** 2 + self.vel[1] ** 2)  # scalar speed
+    def get_speed(self, velocity=None):
+        if velocity is None:
+            velocity = self.vel
+        return math.sqrt(velocity[0] ** 2 + velocity[1] ** 2)  # scalar speed
 
+    def get_direction_vector(self, velocity=None):
+        if velocity is None:
+            velocity = self.vel
+        speed = self.get_speed(velocity)
         if self.speed != 0:
-            self.direction_vector = self.vel / self.speed
+            return velocity / speed
+        else:
+            return self.direction_vector
 
-        # calculate rotation
-        self.rotation = math.degrees(math.atan2(self.direction_vector[0], -self.direction_vector[1])) - 90  # rotation from direction vector
+    def get_rotation(self, direction_vector=None):         # rotation from direction vector (or velocity)
+        if direction_vector is None:
+            direction_vector = self.direction_vector
+
+        if self.reverse:
+            return math.degrees(math.atan2(direction_vector[0], -direction_vector[1])) + 90
+        else:
+
+            return math.degrees(math.atan2(direction_vector[0], -direction_vector[1])) - 90
+
+    def update(self):
+        self.speed = self.get_speed()
+        self.direction_vector = self.get_direction_vector()
+        self.rotation = self.get_rotation()
+
+        if self.speed == 0:
+            self.stopped = True
+        else:
+            self.stopped = False
+
+        """    def forward(self, throttle: float = 0, delta_time: float = 1/FPS, tick=-1):
+        if self.reverse:
+            return self.breaking(throttle, delta_time, tick)
+        else:
+            return self.gas( throttle, delta_time, tick)
+
+    def backward(self, throttle: float = 0, delta_time: float = 1/FPS, tick=-1):
+        if self.reverse:
+            return self.gas(throttle, delta_time, tick)
+        else:
+            return self.breaking(throttle, delta_time, tick)"""
+
+    def get_acceleration(self, throttle: float = 0):
 
         # calculate acceleration
-        traction_force = self.direction_vector * gas * self.engine_max_force
+        traction_force = self.direction_vector * throttle * self.engine_max_force
+        drag = - self.drag_coef * self.vel * self.speed     # -C*v^2 but 2d vector
+        friction = - self.friction_coef * self.vel          # -C*v but 2d vector
+
+        net_force = traction_force + drag + friction
+
+        # engine braking
+        if throttle == 0:
+            net_force -= self.engin_braking * self.direction_vector
+
+        acceleration = net_force/self.mass
+
+        return acceleration
+
+    """
+    def breaking(self, throttle: float = 0, delta_time: float = 1/FPS, tick = -1):
+        if self.stopped:
+            self.reverse = True
+
+        # calculate acceleration
+        traction_force = -self.direction_vector * throttle * self.engine_max_force
         drag = - self.drag_coef * self.vel * self.speed  # -C*v^2 but 2d vector
         friction = - self.friction_coef * self.vel
-        self.acceleration = (traction_force + drag + friction) / self.mass
+        acceleration = (traction_force + drag + friction) / self.mass
 
-        # new speed
-        new_rotation = math.degrees(math.atan2(self.direction_vector[0], -self.direction_vector[1])) - 90
-        change_in_rotation = abs(new_rotation - self.rotation)
-        if self.rotation > 90:      # vel has been reversed
-            self.vel = 0
+        return acceleration"""
+
+    def set_new_velocity_state(self, new_velocity, throttle):          # change name later
+
+        if self.stopped:
+            if throttle > 0:
+                self.reverse = False
+                self.stopped = False
+                self.vel = new_velocity
+            elif throttle < 0:
+                self.reverse = True
+                self.stopped = False
+                self.vel = new_velocity
+            self.vel = np.zeros(2)
         else:
-            self.vel += self.acceleration * delta_time
+            new_rotation = self.get_rotation(new_velocity)
+            change_in_rotation = abs(new_rotation - self.rotation)
+            if change_in_rotation > 90:      # vel has been reversed
+                print("reversed -> stopped")
+                self.vel = np.zeros(2)
+                self.stopped = True
+            else:
+                self.vel = new_velocity
+
+
+    def move(self, throttle: float = 0, turn: float = 0, delta_time: float = 1 / FPS, tick=-1):
+        # update all variables
+        self.update()
+
+        # calculate acceleration
+        acceleration = self.get_acceleration(throttle)
+
+        # new velocity
+        new_vel = self.vel + acceleration * delta_time
+        self.set_new_velocity_state(new_vel, throttle)
+
         # new position
         self.pos += self.vel * delta_time
 
-        print(f"{time}: speed= {self.speed}, vel= {self.vel}")
+        self.update()
+
+        print(f"{tick}: vel= {self.vel}, stopped= {self.stopped}, reverse= {self.reverse}")
 
 
         #self.rotation += turn * self.rotation_speed * delta_time
@@ -99,7 +192,7 @@ def main():
     win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
     clock = pygame.time.Clock()
 
-    car = Car((100, 100))
+    car = car_class.Car((100, 100), CAR_IMG)
 
     run = True
     while run:
@@ -113,13 +206,12 @@ def main():
                 pygame.quit()
                 quit()
         if keyboard.is_pressed("z"):
-            gas = 1
+            throttle = 1
         elif keyboard.is_pressed("s"):
-            gas = -1
+            throttle = -1
         else:
-            gas = 0
-
-        car.move(gas, 0, delta_time, pygame.time.get_ticks())
+            throttle = 0
+        car.move(throttle, 0, delta_time, pygame.time.get_ticks())
 
         # draw window
         draw_window(win, car)
